07c04f45295e7b41bb4404f3c005613c6e54d266
wcchen1108 <s1091759@gm.pu.edu.tw>
2022-06-17 22:57:24 +0800

    Control Snake

--- app/src/main/java/tw/edu/pu/csim/mis2b/wcchen/snake/GameView.java
+++ app/src/main/java/tw/edu/pu/csim/mis2b/wcchen/snake/GameView.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.os.Handler;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -17,6 +19,10 @@ public class GameView extends View {
     private int h = 21, w = 12;
     private ArrayList<Grass> arrGrass = new ArrayList<>();
     private Snake snake;        //Create Snake in GameView
+    private boolean move = false;
+    private float mx, my;
+    private Handler handler;
+    private Runnable r;
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         bmGrass1 = BitmapFactory.decodeResource(this.getResources(), R.drawable.grass);
@@ -37,6 +43,53 @@ public class GameView extends View {
             }
         }
         snake = new Snake(bmSnake, arrGrass.get(126).getX(), arrGrass.get(126).getY(), 4);
+         handler = new Handler();
+         r = new Runnable(){
+             @Override
+             public void run(){
+                 invalidate();
+             }
+         };
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int a = event.getActionMasked();
+        switch (a){
+            case MotionEvent.ACTION_MOVE:{
+                if (move == false){
+                    mx = event.getX();
+                    my = event.getY();
+                    move = true;
+                }else{
+                    if (mx - event.getX() > 100*Constants.SCREEN_WIDTH/1080 && !snake.isMove_right()){
+                        mx = event.getX();
+                        my = event.getY();
+                        snake.setMove_left(true);
+                    }else if (event.getX() -mx > 100*Constants.SCREEN_WIDTH/1080 && !snake.isMove_left()){
+                        mx = event.getX();
+                        my = event.getY();
+                        snake.setMove_right(true);
+                    }else if (my - event.getY() > 100*Constants.SCREEN_WIDTH/1080 && !snake.isMove_bottom()){
+                        mx = event.getX();
+                        my = event.getY();
+                        snake.setMove_top(true);
+                    }else if (event.getY() - my  > 100*Constants.SCREEN_WIDTH/1080 && !snake.isMove_top()){
+                        mx = event.getX();
+                        my = event.getY();
+                        snake.setMove_bottom(true);
+                    }
+                }
+                break;
+            }
+            case MotionEvent.ACTION_UP:{
+                mx = 0;
+                my = 0;
+                move = false;
+                break;
+            }
+        }
+        return true;
     }
 
     @Override
@@ -46,6 +99,8 @@ public class GameView extends View {
         for (int i = 0; i < arrGrass.size(); i++){
             canvas.drawBitmap(arrGrass.get(i).getBm(), arrGrass.get(i).getX(), arrGrass.get(i).getY(), null);
         }
+        snake.update();
         snake.draw(canvas);
+        handler.postDelayed(r, 100);
     }
 }
--- app/src/main/java/tw/edu/pu/csim/mis2b/wcchen/snake/Snake.java
+++ app/src/main/java/tw/edu/pu/csim/mis2b/wcchen/snake/Snake.java
@@ -6,6 +6,7 @@ import android.graphics.Canvas;
 import java.util.ArrayList;
 
 public class Snake {
+    private boolean move_left, move_right, move_top, move_bottom;
     private Bitmap bm, bm_head_up, bm_head_down, bm_head_left, bm_head_right, bm_body_vertical, bm_body_horizontal,
     bm_body_top_right, bm_body_top_left, bm_body_bottom_right, bm_body_bottom_left, bm_tail_right, bm_tail_left,
     bm_tail_up, bm_tail_down;
@@ -36,13 +37,75 @@ public class Snake {
             arrPartSnake.add(new PartSnake(bm_body_horizontal, arrPartSnake.get(i-1).getX() - GameView.size0fMap, y));      //Pic:Snake_body
         }
         arrPartSnake.add(new PartSnake(bm_tail_right, arrPartSnake.get(length-2).getX() - GameView.size0fMap, y));      //Pic:Snake_tail
+        setMove_right(true);
     }
 
-    public void draw(Canvas canvas){
-        for (int i = 0; i < length; i++){
-            canvas.drawBitmap(arrPartSnake.get(i).getBm(), arrPartSnake.get(i).getX(), arrPartSnake.get(i).getY(), null);
+    public void update() {       //Pic:Update
+        for (int i = length - 1; i > 0; i--) {
+            arrPartSnake.get(i).setX(arrPartSnake.get(i - 1).getX());
+            arrPartSnake.get(i).setY(arrPartSnake.get(i - 1).getY());
+        }
+        if (move_right) {       //移動時的X,Y分別增加、減少
+            arrPartSnake.get(0).setX(arrPartSnake.get(0).getX() + GameView.size0fMap);
+            arrPartSnake.get(0).setBm(bm_head_right);
+        } else if (move_left) {
+            arrPartSnake.get(0).setX(arrPartSnake.get(0).getX() - GameView.size0fMap);
+            arrPartSnake.get(0).setBm(bm_head_left);
+        } else if (move_top) {
+            arrPartSnake.get(0).setY(arrPartSnake.get(0).getY() - GameView.size0fMap);
+            arrPartSnake.get(0).setBm(bm_head_up);
+        } else if (move_bottom) {
+            arrPartSnake.get(0).setY(arrPartSnake.get(0).getY() + GameView.size0fMap);
+            arrPartSnake.get(0).setBm(bm_head_down);
+        }
+        for (int i = 1; i < length - 1; i++){
+            if (arrPartSnake.get(i).getrLeft().intersect(arrPartSnake.get(i+1).getrBody())
+            &&arrPartSnake.get(i).getrBottom().intersect(arrPartSnake.get(i-1).getrBody())
+            ||arrPartSnake.get(i).getrLeft().intersect(arrPartSnake.get(i-1).getrBody())
+                    &&arrPartSnake.get(i).getrBottom().intersect(arrPartSnake.get(i+1).getrBody())){
+                arrPartSnake.get(i).setBm(bm_body_bottom_left);
+            }else if (arrPartSnake.get(i).getrRight().intersect(arrPartSnake.get(i+1).getrBody())
+                    &&arrPartSnake.get(i).getrBottom().intersect(arrPartSnake.get(i-1).getrBody())
+                    ||arrPartSnake.get(i).getrRight().intersect(arrPartSnake.get(i-1).getrBody())
+                    &&arrPartSnake.get(i).getrBottom().intersect(arrPartSnake.get(i+1).getrBody())){
+                arrPartSnake.get(i).setBm(bm_body_bottom_right);
+            }else if (arrPartSnake.get(i).getrLeft().intersect(arrPartSnake.get(i+1).getrBody())
+                    &&arrPartSnake.get(i).getrTop().intersect(arrPartSnake.get(i-1).getrBody())
+                    ||arrPartSnake.get(i).getrLeft().intersect(arrPartSnake.get(i-1).getrBody())
+                    &&arrPartSnake.get(i).getrTop().intersect(arrPartSnake.get(i+1).getrBody())){
+                arrPartSnake.get(i).setBm(bm_body_top_left);
+            }else if (arrPartSnake.get(i).getrRight().intersect(arrPartSnake.get(i+1).getrBody())
+                    &&arrPartSnake.get(i).getrTop().intersect(arrPartSnake.get(i-1).getrBody())
+                    ||arrPartSnake.get(i).getrRight().intersect(arrPartSnake.get(i-1).getrBody())
+                    &&arrPartSnake.get(i).getrTop().intersect(arrPartSnake.get(i+1).getrBody())){
+                arrPartSnake.get(i).setBm(bm_body_top_right);
+            }else if (arrPartSnake.get(i).getrTop().intersect(arrPartSnake.get(i+1).getrBody())
+                    &&arrPartSnake.get(i).getrBottom().intersect(arrPartSnake.get(i-1).getrBody())
+                    ||arrPartSnake.get(i).getrTop().intersect(arrPartSnake.get(i-1).getrBody())
+                    &&arrPartSnake.get(i).getrBottom().intersect(arrPartSnake.get(i+1).getrBody())){
+                arrPartSnake.get(i).setBm(bm_body_vertical);
+            }else if (arrPartSnake.get(i).getrLeft().intersect(arrPartSnake.get(i+1).getrBody())
+                    &&arrPartSnake.get(i).getrRight().intersect(arrPartSnake.get(i-1).getrBody())
+                    ||arrPartSnake.get(i).getrLeft().intersect(arrPartSnake.get(i-1).getrBody())
+                    &&arrPartSnake.get(i).getrRight().intersect(arrPartSnake.get(i+1).getrBody())){
+                arrPartSnake.get(i).setBm(bm_body_horizontal);
+            }
+        }
+        if (arrPartSnake.get(length-1).getrRight().intersect(arrPartSnake.get(length-2).getrBody())){
+            arrPartSnake.get(length-1).setBm(bm_tail_right);
+        }else if (arrPartSnake.get(length-1).getrLeft().intersect(arrPartSnake.get(length-2).getrBody())){
+            arrPartSnake.get(length-1).setBm(bm_tail_left);
+        }else if (arrPartSnake.get(length-1).getrTop().intersect(arrPartSnake.get(length-2).getrBody())){
+            arrPartSnake.get(length-1).setBm(bm_tail_up);
+        }else if (arrPartSnake.get(length-1).getrBottom().intersect(arrPartSnake.get(length-2).getrBody())){
+            arrPartSnake.get(length-1).setBm(bm_tail_down);
         }
     }
+        public void draw(Canvas canvas){
+            for (int i = 0; i < length; i++){
+                canvas.drawBitmap(arrPartSnake.get(i).getBm(), arrPartSnake.get(i).getX(), arrPartSnake.get(i).getY(), null);
+            }
+        }
 
     public Bitmap getBm() {
         return bm;
@@ -194,5 +257,48 @@ public class Snake {
 
     public void setArrPartSnake(ArrayList<PartSnake> arrPartSnake) {
         this.arrPartSnake = arrPartSnake;
+    }
+
+    public boolean isMove_left() {
+        return move_left;
+    }
+
+    public void setMove_left(boolean move_left) {
+        s();
+        this.move_left = move_left;
+    }
+
+    public boolean isMove_right() {
+        return move_right;
+    }
+
+    public void setMove_right(boolean move_right) {
+        s();
+        this.move_right = move_right;
+    }
+
+    public boolean isMove_top() {
+        return move_top;
+    }
+
+    public void setMove_top(boolean move_top) {
+        s();
+        this.move_top = move_top;
+    }
+
+    public boolean isMove_bottom() {
+        return move_bottom;
+    }
+
+    public void setMove_bottom(boolean move_bottom) {
+        s();
+        this.move_bottom = move_bottom;
+    }
+
+    public void s(){
+        this.move_left = false;
+        this.move_right = false;
+        this.move_bottom = false;
+        this.move_top = false;
     }
 }
